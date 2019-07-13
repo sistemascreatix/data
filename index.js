63b7bffd4f79f6191133bda2712d4b57f4a66b73
Delfin <luis.delfin.bg@gmail.com>
2019-07-12 22:00:46 -0400

    show full country data

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var listatranslations = [];
 paisesDet.map(({ name, flag, nativeName, alpha2Code, latlng, timezones, numericCode, currencies, languages, translations }, index) => {
 
     [lat, lng] = latlng || [0, 0];
-    listapaises[alpha2Code] = { 'Id': index, name, flag, nativeName, alpha2Code, lat, lng, timezones: JSON.stringify(timezones), numericCode };
+    listapaises[alpha2Code] ={ 'Id': index, name, flag, nativeName, alpha2Code, lat, lng, timezones: JSON.stringify(timezones), numericCode };
 
     let nuevatranslations = Object.entries(translations).map(([key, value]) => {
         return { 'countryId': index, key, value };
@@ -17,11 +17,11 @@ paisesDet.map(({ name, flag, nativeName, alpha2Code, latlng, timezones, numericC
     listatranslations = [...listatranslations, ...nuevatranslations]
 
     currencies.map(({ code, name, symbol }) => {
-        listacurrencies[code] = { 'countryId': index, code, name, symbol }
+        listacurrencies =[...listacurrencies, { 'countryId': index, code, name, symbol }]
     })
 
     languages.map(({ iso639_1, iso639_2, name, nativeName }) => {
-        listalanguages[iso639_1] = { 'countryId': index, iso639_1, iso639_2, name, nativeName }
+        listalanguages =[...listalanguages,{ 'countryId': index, iso639_1, iso639_2, name, nativeName }]
     })
 })
 
@@ -39,12 +39,14 @@ listaciudades = ciudadesDet.map(({ CIUDAD, alpha2Code }) => (listapaises[alpha2C
         lng: 0,
     }));
 
-// let arraypaises = Object.entries(listapaises);
-// console.log(arraypaises);
-//console.log(listaciudades.filter(x => x.alpha2Code == "BO"))
-//console.log(listacurrencies)
-//console.log(listalanguages);
-console.log(listatranslations);
+    //  Show data 
+let filtercode = 'BO';
+let pais = listapaises[filtercode];
+console.table(listaciudades.filter(city => city.alpha2Code == filtercode))
+console.table(pais);
+console.table(listacurrencies.filter(currency =>currency.countryId == pais.Id))
+console.table(listalanguages.filter(language => language.countryId == pais.Id));
+console.table(listatranslations.filter(translation => translation.countryId == pais.Id));
 // fetch('http://tucomunidadvirtual.com/')
 //     .then(res => res.text())
 //     .then(body => console.log(body));
