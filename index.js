1067fd6b1b2b022a022336b641f3a0995b4839c5
Delfin <luis.delfin.bg@gmail.com>
2019-10-29 17:33:28 -0400

    29102019

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {listapaises,
        listalanguages,
        listapaisLanguages,
        listatranslations,
-        listaciudades} = require('./src/parser');
+       listaciudades} = require('./src/parser');
 
        writeFile.writeFile('./Files/paises.csv',objectToCSV.objectToCSV(listapaises));
        writeFile.writeFile('./Files/currencies.csv',objectToCSV.objectToCSV(listacurrencies));
@@ -32,3 +32,17 @@ const {listapaises,
 // fetch('http://tucomunidadvirtual.com/')
 //     .then(res => res.text())
 //     .then(body => console.log(body));
+
+
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
+ 
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
--- src/parser/index.js
+++ src/parser/index.js
@@ -1,6 +1,17 @@
 const paisesDet = require('../../data/Paises_Detail.json')
 const ciudadesDet = require('../../data/Ciudades.json')
 // module.exports = exports = {}
+
+function cleanstring(value) {
+    
+    let remplaza=(value)=>{
+        return value.replace(/[\/\\^$*+?&,"()|[\]{}:“”]/g, '').replace(/'/g, '`');
+    }
+    
+    return `'${remplaza(`${value}`)}'`;
+
+}
+
 var listapaises = [];
 var listacurrencies = [];
 var listapaiscurrencies = [];
@@ -8,39 +19,41 @@ var listalanguages = [];
 var listapaisLanguages = [];
 var listatranslations = [];
 var listapaises2 = [];
+var geoblocksFormatted=[];
 var counterLang = 0;
 var counterCurr = 0;
 paisesDet.map(({ name, flag, nativeName,capital,region,subregion, alpha2Code, latlng, timezones, numericCode, currencies, languages, translations }, index) => {
     index++
     [lat, lng] = latlng || [0, 0];
-    listapaises.push( { 'Id': index, name,capital,region,subregion, flag, nativeName, alpha2Code, lat, lng, timezones: JSON.stringify(timezones), numericCode });
-    listapaises2[alpha2Code]= { 'Id': index, name,capital,region,subregion, flag, nativeName, alpha2Code, lat, lng, timezones: JSON.stringify(timezones), numericCode };
-
+    listapaises.push( { 'Id': index,"GeoBlockTypeId":1, name,capital,region,subregion, flag, nativeName, alpha2Code, lat, lng, timezones: JSON.stringify(timezones), numericCode });
+    listapaises2[alpha2Code]= { 'Id': index, "GeoBlockTypeId":1,name,capital,region,subregion, flag, nativeName, alpha2Code, lat, lng, timezones: JSON.stringify(timezones), numericCode };
+    geoblocksFormatted.push( { 'Id': index,"GeoBlockTypeId":1, "LongName" : cleanstring(name) ,"ShortName": `'${alpha2Code}'`, "Latitude": lat,"Longitude": lng, "formattedaddress":cleanstring(name),"North":0,
+    "South":0,"East":0,"West":0,"ParentId":null,"CreatedAt":"2019-10-14","UpdatedAt":"2019-10-14"});
     let nuevatranslations = Object.entries(translations).map(([key, value]) => {
-        return { 'countryId': index, key, value };
+        return { 'countryId': index, key: `'${key}'`,'value' : cleanstring(value) };
     });
     listatranslations = [...listatranslations, ...nuevatranslations]
 
     currencies.map(({ code, name, symbol }) => {
-        let k = listacurrencies.find(c => c.code == code);
+        let k = listacurrencies.find(c => c.code == cleanstring(code));
         if (!k) {
             counterCurr++;
-            listacurrencies.push({ Id: counterCurr, code, name, symbol });
-            listapaiscurrencies.push({ 'countryId': index, currencyId: counterCurr })
+            listacurrencies.push({ Id: counterCurr,'code':cleanstring(code), 'name':cleanstring(name),'symbol':cleanstring(symbol),'valid':1 });
+            listapaiscurrencies.push({ 'countryId': index, currencyId: counterCurr,"CreatedAt": cleanstring("2019-10-14"),"UpdatedAt":cleanstring("2019-10-14") })
         } else {
-            listapaiscurrencies.push({ 'countryId': index, currencyId: k.Id })
+            listapaiscurrencies.push({ 'countryId': index, currencyId: k.Id,"CreatedAt": cleanstring("2019-10-14"),"UpdatedAt":cleanstring("2019-10-14") })
         }
 
     })
 
-    languages.map(({ iso639_1, iso639_2, name, nativeName }) => {
+    languages.map(({ iso639_1, iso639_2,name,nativeName }) => {
         let k = listalanguages.find(lang => lang.iso639_1 == iso639_1);
         if (!k) {
             counterLang++;
-            listalanguages.push({ Id: counterLang, iso639_1, iso639_2, name, nativeName });
-            listapaisLanguages.push({ 'countryId': index, languageId: counterLang });
+            listalanguages.push({ Id: counterLang,'alpha2code': cleanstring(iso639_1), 'name':cleanstring(name),'nativeName': cleanstring(nativeName) });
+            listapaisLanguages.push({ languageId: counterLang ,'countryId': index });
         } else {
-            listapaisLanguages.push({ 'countryId': index, languageId: k.Id });
+            listapaisLanguages.push({ languageId: k.Id, 'countryId': index });
         }
 
     })
@@ -48,14 +61,16 @@ paisesDet.map(({ name, flag, nativeName,capital,region,subregion, alpha2Code, la
 
 listaciudades = ciudadesDet.map(({ CIUDAD, alpha2Code }) => (listapaises2[alpha2Code] ? {
     countryId: listapaises2[alpha2Code].Id,
-    name: CIUDAD,
-    alpha2Code,
+    name: cleanstring(CIUDAD),
+    alpha2Code:cleanstring(alpha2Code),
     lat: listapaises2[alpha2Code].lat,
     lng: listapaises2[alpha2Code].lng,
 } : {}));
 
 
-exports.listapaises = listapaises;
+
+
+exports.listapaises = geoblocksFormatted;
 exports.listacurrencies = listacurrencies;
 exports.listapaiscurrencies = listapaiscurrencies;
 exports.listalanguages = listalanguages;
